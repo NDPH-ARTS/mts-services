ca2cc7a571cfe67b889920de57ed3871f081048c
Itye Richter <itye.richter@microsoft.com>
2020-12-29 18:32:47 +0200

    Added parent pom (#7)
    * parent pom
    
    * fix https
    
    * pom fix
    
    * package rename
    
    * CI changes
    
    * CI changes
    
    * CI changes
    
    * CI changes docker
    
    * CI changes docker
    
    * CI changes docker
    
    * refactor interface
    
    Co-authored-by: Itye <itye@ityer.guest.corp.microsoft.com>
    Co-authored-by: Itye <itye@ityer-2.local>

--- sample-service/src/main/java/com/microsoft/baseapp/config/IConfigService.java
+++
@@ -1,5 +0,0 @@
-package com.microsoft.baseapp.config;
-
-public interface IConfigService {
-    String getSecret();
-}
---
+++ sample-service/src/main/java/uk/ac/ox/ndph/mts/sample_service/config/ConfigService.java
@@ -0,0 +1,5 @@
+package uk.ac.ox.ndph.mts.sample_service.config;
+
+public interface ConfigService {
+    String getSecret();
+}
