8872248486bff101da0158952dc49a7ec3635d6a
Darren Smithson <darrengsmithson@hotmail.com>
2021-01-04 15:32:25 +0000

    Arts 265 (#13)
    * [ARTS-265]
    Removing security config for the service as this will be an internal call and any security will be external to these files.
    
    * [ARTS-265]
    Removing security config for the service as this will be an internal call and any security will be external to these files. Fixing merge conflicts
    
    * [ARTS-265]
    Removing security config for the service as this will be an internal call and any security will be external to these files. Fixing merge conflicts
    
    * [ARTS-265]
    Removing security config for the service as this will be an internal call and any security will be external to these files. Fixing merge conflicts. Readding swagger code from webconfig - missed during the auth purge.

--- trial-config-service/src/main/java/uk/ac/ox/ndph/mts/trial_config_service/config/WebConfig.java
+++ trial-config-service/src/main/java/uk/ac/ox/ndph/mts/trial_config_service/config/WebConfig.java
@@ -8,72 +8,20 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.*;
-import springfox.documentation.service.*;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.SecurityConfiguration;
-import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static java.util.Arrays.asList;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer, WebConfigService {
-
-    private static final String API_MTS_APPS_ADMIN = "api://mts-apps/admin";
-    private static final String ADMIN_SCOPE = "admin scope";
-
-    // TODO ARTS-264 put these values into Azure Key Vault
-    @Value("${spring.security.oauth2.resourceserver.opaquetoken.client-secret}")
-    private String clientSecret;
-
-    // TODO ARTS-264 put these values into Azure Key Vault
-    @Value("${spring.security.oauth2.resourceserver.opaquetoken.client-id}")
-    private String clientId;
-
-    // TODO ARTS-264 put these values into Azure Key Vault
-    @Value("${swagger.authserver.url}")
-    private String authServer;
-
-    private List<AuthorizationScope> authorizationScopeList;
-
-    WebConfig() {
-        authorizationScopeList = new ArrayList<>();
-    }
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket api() {
-        authorizationScopeList.add(new AuthorizationScope(API_MTS_APPS_ADMIN, ADMIN_SCOPE));
-
-
         return new Docket(DocumentationType.SWAGGER_2).select().apis(
             RequestHandlerSelectors.basePackage("uk.ac.ox.ndph.mts.trial_config_service.controller"))
-                                                      .paths(PathSelectors.any()).build()
-                                                      .securitySchemes(asList(securityScheme()))
-                                                      .securityContexts(asList(securityContext()));
-    }
-
-    @Bean
-    public SecurityConfiguration security() {
-        return SecurityConfigurationBuilder.builder().clientId(clientId).clientSecret(clientSecret)
-                                           .scopeSeparator(" ").useBasicAuthenticationWithAccessCodeGrant(true).build();
-    }
-
-    private SecurityScheme securityScheme() {
-        TokenRequestEndpoint token = new TokenRequestEndpointBuilder().url(authServer + "/authorize").build();
-        TokenEndpoint authToken = new TokenEndpointBuilder().url(authServer + "/token").build();
-        GrantType grantType = new AuthorizationCodeGrant(token, authToken);
-
-        return new OAuthBuilder().grantTypes(asList(grantType)).scopes(authorizationScopeList).name("azure").build();
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder().securityReferences(
-            asList(new SecurityReference("azure", authorizationScopeList.toArray(new AuthorizationScope[]{})))).build();
+                                                      .paths(PathSelectors.any()).build();
     }
 
     @Bean
@@ -99,18 +47,4 @@ public class WebConfig implements WebMvcConfigurer, WebConfigService {
         return WebClient.create();
     }
 
-    @Override
-    public String getClientSecret() {
-        return clientSecret;
-    }
-
-    @Override
-    public String getClientId() {
-        return clientId;
-    }
-
-    @Override
-    public String getAuthServer() {
-        return authServer;
-    }
 }
