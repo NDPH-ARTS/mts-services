7b6761a517f8ecfd0bcde1f44515f232cbe64031
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2021-01-04 13:38:16 +0200

    Enable checkstyle for all projects (#14)
    * add checkstyle
    
    * style fix in sample-service
    
    * style fix in practitioner-service
    
    * update docker files
    
    * remove mvnw
    
    * remove spring style from parent pom
    
    * update workflow
    
    * remove comment from pom

--- practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/PractitionerServiceApp.java
+++ practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/PractitionerServiceApp.java
@@ -10,24 +10,36 @@ import org.springframework.http.HttpStatus;
 import uk.ac.ox.ndph.mts.practitioner_service.model.Practitioner;
 import uk.ac.ox.ndph.mts.practitioner_service.service.EntityService;
 
+/**
+ *
+ */
 @RestController
 @SpringBootApplication
 public class PractitionerServiceApp {
-	
-	private static final String ENDPOINT_PATH = "/practitioner";
-	private static final String APPLICATION_JSON = "application/json";
-	private static final String RESPONSE = "{\"id\": \"%s\"}";
 
-	private EntityService entityService;
+    private static final String ENDPOINT_PATH = "/practitioner";
+    private static final String APPLICATION_JSON = "application/json";
+    private static final String RESPONSE = "{\"id\": \"%s\"}";
 
-	@Autowired
-	public PractitionerServiceApp(EntityService entityService) {
-		this.entityService = entityService;
-	}
+    private final EntityService entityService;
 
-	@PostMapping(path = ENDPOINT_PATH, consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
-	public ResponseEntity<String> practitioner(@RequestBody Practitioner practitioner) {
-		String practitionerId = entityService.savePractitioner(practitioner);
-		return ResponseEntity.status(HttpStatus.CREATED).body(String.format(RESPONSE, practitionerId));
-	}
+    /**
+     *
+     * @param entityService
+     */
+    @Autowired
+    public PractitionerServiceApp(EntityService entityService) {
+        this.entityService = entityService;
+    }
+
+    /**
+     *
+     * @param practitioner
+     * @return ResponseEntity
+     */
+    @PostMapping(path = ENDPOINT_PATH, consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    public ResponseEntity<String> practitioner(@RequestBody Practitioner practitioner) {
+        String practitionerId = entityService.savePractitioner(practitioner);
+        return ResponseEntity.status(HttpStatus.CREATED).body(String.format(RESPONSE, practitionerId));
+    }
 }
--- sample-service/src/main/java/uk/ac/ox/ndph/mts/sample_service/Main.java
+++ sample-service/src/main/java/uk/ac/ox/ndph/mts/sample_service/Main.java
@@ -4,8 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 
+/**
+ * The main class of this service.
+ */
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    /**
+     * The entry point for this class.
+     * @param args command line args
+     */
     public static void main(String[] args) {
 
         LOGGER.info("Staring base app...");
