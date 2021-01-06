508234ddb35994981861726b82102b5f73abc660
Darren Smithson <33603539+darrengsmithson@users.noreply.github.com>
2021-01-06 16:20:54 +0000

    [ARTS-291] Removing git directory creation to check if directory already exists

--- trial-config-service/src/test/java/uk/ac/ox/ndph/mts/trial_config_service/config/GitRepoTest.java
+++ trial-config-service/src/test/java/uk/ac/ox/ndph/mts/trial_config_service/config/GitRepoTest.java
@@ -1,6 +1,5 @@
 package uk.ac.ox.ndph.mts.trial_config_service.config;
 
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
