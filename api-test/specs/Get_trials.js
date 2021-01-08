d210468112e827f2f62d755006eb1801ac6791c7
Mark Phipps <mark.phipps0@gmail.com>
2021-01-08 16:17:38 +0000

    First cut of test and compose file

--- api-test/specs/Get_trials.js
+++ api-test/specs/Get_trials.js
@@ -8,10 +8,16 @@ beforeEach(function () {
   baseRequest = request(conf.BASE_URL)
 })
 
-describe('Trial Config Service', function () {
-  it(' Should initialise a new trial from a valid config URL', async function () {
-    const response = await baseRequest.post("/trial-config/trial").send('http://global-trial-config-mock:8085/trial/test_1')
-    expect(response.status).to.equal(HttpStatus.OK)
+describe('Practitioner Service', function () {
+  let person = {
+    prefix: 'XXXXX',
+    givenName: 'John',
+    familyName: 'Gamily'
+  };
+
+  it('Should create a new FHIR practitioner', async function () {
+    const response = await baseRequest.post("/practitioner").send(person);
+    expect(response.status).to.equal(201);
   });
 
 });
