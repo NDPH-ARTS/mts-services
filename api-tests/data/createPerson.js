0c734e81897e8ffe09a7c54d33d39813e2ef7b95
Sameera Purini <purinisameera@gmail.com>
2021-01-10 21:06:24 +0000

    arts-184-create-person-validation-api-tests

---
+++ api-tests/data/createPerson.js
@@ -0,0 +1,17 @@
+const validPerson = {
+    "prefix": "Mr",
+    "givenName": "Fermer",
+    "familyName": "Bonjour",
+}
+
+const invlidPerson = {
+    "prefix": "",
+    "givenName": "",
+    "familyName": "",
+}
+
+const invalidCharacterLength = {
+    "prefix": "",
+    "givenName": "",
+    "familyName": "ghfghdfgdhfgdhgfdhfgdhgfhdgfhdgfhdgfdhgfhdgfhdgfhdgfhfgdhfgdhf",
+}
