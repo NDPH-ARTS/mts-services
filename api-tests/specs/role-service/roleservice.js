be9df441cf0550f86e67ed897e3a0160b71d36cb
Sameera Purini <purinisameera@gmail.com>
2021-01-20 17:27:12 +0000

    Updated the status for trial config responses

--- api-tests/specs/role-service/roleservice.js
+++ api-tests/specs/role-service/roleservice.js
@@ -7,45 +7,45 @@ beforeEach(function () {
 })
 
 describe('As a user I want to create roles so that they can be assigned to persons', function () {
-    it.only('I am able to add a role to the trial site successfully', async () => {
+    it('I am able to add a role to the trial site successfully', async () => {
         const response = await baseRequest.post('/roles').send(requests.validRole);
         expect(response.text).to.contain("id")
         expect(response.status).to.equal(HttpStatus.OK)
     });
 
-    it.only('User is prevented from saving the new role if the name is already used for an existing role', async () => {
+    it('User is prevented from saving the new role if the name is already used for an existing role', async () => {
         const response = await baseRequest.post('/roles').send(requests.duplicateRole);
         expect(response.status).to.equal(409)
         expect(response.text).to.contain("Duplicate role ID")
     });
 
-    it.only('User is shown a bad request error when the role name field is left empty', async () => {
+    it('User is shown a bad request error when the role name field is left empty', async () => {
         const response = await baseRequest.post('/roles').send(requests.emptyString);
         expect(response.status).to.equal(400);
         expect(response.text).to.contain("Role ID cannot be empty")
     });
 
-    it.only('User is shown a bad request error when the role name field exceeds the expected string length', async () => {
+    it('User is shown a bad request error when the role name field exceeds the expected string length', async () => {
         const response = await baseRequest.post('/roles').send(requests.tooLongId);
         expect(response.status).to.equal(400);
         expect(response.text).to.contain("Role ID is too long")
     });
 
-    it.only('User is able to view the created roles', async () => {
+    it('User is able to view the created roles', async () => {
         const response = await baseRequest.get('/roles?page=1&size=2');
         expect(response.status).to.equal(HttpStatus.OK)
         expect(response.text).to.contain("id");
     });
 
-    it.only('User is able to create one or more roles via the trial config service', async () => {
+    it('User is able to create one or more roles via the trial config service', async () => {
         const response = await trialConfigService.post('/trial-config/trial?filename=1').send(requests.trialConfigPost);
-        expect(response.status).to.equal(500)
+        expect(response.status).to.equal(HttpStatus.OK)
         expect(response.text).to.contain("id");
     });
 
-    it.only('User is able to GET the roles created via the trial config service', async () => {
+    it('User is able to GET the roles created via the trial config service', async () => {
         const response = await trialConfigService.get('/trial-config/trial?filename=1');
-        expect(response.status).to.equal(500)
+        expect(response.status).to.equal(HttpStatus.OK)
         expect(response.text).to.contain("id");
     });
 
