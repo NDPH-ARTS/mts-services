456c305bafd944f28872c404e34e0bfb74cd9fa0
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2021-01-21 13:22:33 +0200

    fix mock tsets

---
+++ sc-config-server/src/test/java/uk/ac/ox/ndph/mts/sc_config_server/MtsSpringConfigServerApplicationTests.java
@@ -0,0 +1,30 @@
+package uk.ac.ox.ndph.mts.sc_config_server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MtsSpringConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ConfigService configService;
+
+}
