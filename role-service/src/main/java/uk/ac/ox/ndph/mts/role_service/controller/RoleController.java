00eb756037f0007820c49b9d46625db700d2bfdf
Kate Sant <katesan@ndph.ox.ac.uk>
2021-01-21 15:30:25 +0000

    [ARTS-101] more checkstyle cleanup

--- role-service/src/main/java/uk/ac/ox/ndph/mts/role_service/controller/RoleController.java
+++ role-service/src/main/java/uk/ac/ox/ndph/mts/role_service/controller/RoleController.java
@@ -25,7 +25,6 @@ import javax.validation.Valid;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 
