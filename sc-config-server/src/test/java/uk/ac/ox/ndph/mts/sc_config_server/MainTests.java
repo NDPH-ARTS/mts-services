2dedf7a6f9d7649c03d8227e14d78e6fb6544dfe
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2021-01-21 13:34:34 +0200

    removing test

--- sc-config-server/src/test/java/uk/ac/ox/ndph/mts/sc_config_server/MainTests.java
+++ sc-config-server/src/test/java/uk/ac/ox/ndph/mts/sc_config_server/MainTests.java
@@ -1,7 +1,9 @@
 package uk.ac.ox.ndph.mts.sc_config_server;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,10 +14,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 @SpringBootTest
 @AutoConfigureMockMvc
-class MtsSpringConfigServerApplicationTests {
+class MainTests {
 
     @Test
     void contextLoads() {
@@ -23,8 +24,5 @@ class MtsSpringConfigServerApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private ConfigService configService;
 
 }
