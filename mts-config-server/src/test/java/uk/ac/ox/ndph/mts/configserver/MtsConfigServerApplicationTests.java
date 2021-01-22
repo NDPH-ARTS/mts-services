0e707d0d609d549e16f4284fafc6fd3a6a9588f2
Darren Smithson <33603539+darrengsmithson@users.noreply.github.com>
2021-01-22 15:20:15 +0000

    [ARTS-423] changes to ensure json is piked up, rename of package, removing properties from service

---
+++ mts-config-server/src/test/java/uk/ac/ox/ndph/mts/configserver/MtsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.ox.ndph.mts.springconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
