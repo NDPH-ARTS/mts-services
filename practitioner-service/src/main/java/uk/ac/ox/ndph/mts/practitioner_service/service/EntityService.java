571057262722659b86df99aa9829fc4346535766
tamirkamara <26870601+tamirkamara@users.noreply.github.com>
2021-01-24 16:01:38 +0200

    A basic implementation for assigning roles to users

--- practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/service/EntityService.java
+++ practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/service/EntityService.java
@@ -1,6 +1,7 @@
 package uk.ac.ox.ndph.mts.practitioner_service.service;
 
 import uk.ac.ox.ndph.mts.practitioner_service.model.Practitioner;
+import uk.ac.ox.ndph.mts.practitioner_service.model.RoleAssignment;
 
 /**
  * Interface for validating and saving an entity
@@ -14,4 +15,6 @@ public interface EntityService {
      * @return the id of the created entity.
      */
     String savePractitioner(Practitioner practitioner);
+
+    String saveRoleAssignment(RoleAssignment roleAssignment);
 }
