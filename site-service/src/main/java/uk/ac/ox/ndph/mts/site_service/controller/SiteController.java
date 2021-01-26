5c3bbe12ae2ee882af71fabf31bce9ff3bb337a1
kedarl <lele.kedar@gmail.com>
2021-01-26 18:24:50 +0000

    07 Fixed Review Comments & CheckStyle Errors

--- site-service/src/main/java/uk/ac/ox/ndph/mts/site_service/controller/SiteController.java
+++ site-service/src/main/java/uk/ac/ox/ndph/mts/site_service/controller/SiteController.java
@@ -1,13 +1,10 @@
 package uk.ac.ox.ndph.mts.site_service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import uk.ac.ox.ndph.mts.site_service.model.Response;
 import uk.ac.ox.ndph.mts.site_service.model.Site;
 import uk.ac.ox.ndph.mts.site_service.service.SiteService;
 
