076d65a87062021dd4b6987b1c6db814e54da077
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2021-01-26 11:01:12 +0200

    temp test

--- practitioner-service/src/test/java/uk/ac/ox/ndph/mts/practitioner_service/controller/PractitionerControllerTests.java
+++ practitioner-service/src/test/java/uk/ac/ox/ndph/mts/practitioner_service/controller/PractitionerControllerTests.java
@@ -1,32 +1,46 @@
 package uk.ac.ox.ndph.mts.practitioner_service.controller;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.http.MediaType;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
-import org.mockito.Mockito;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import uk.ac.ox.ndph.mts.practitioner_service.model.Practitioner;
-import uk.ac.ox.ndph.mts.practitioner_service.service.EntityService;
+import uk.ac.ox.ndph.mts.practitioner_service.configuration.PractitionerConfigurationProvider;
 import uk.ac.ox.ndph.mts.practitioner_service.exception.RestException;
 import uk.ac.ox.ndph.mts.practitioner_service.exception.ValidationException;
-
+import uk.ac.ox.ndph.mts.practitioner_service.model.Practitioner;
+import uk.ac.ox.ndph.mts.practitioner_service.model.PractitionerAttributeConfiguration;
+import uk.ac.ox.ndph.mts.practitioner_service.model.PractitionerConfiguration;
+import uk.ac.ox.ndph.mts.practitioner_service.service.EntityService;
 //@ActiveProfiles("test")
 @SpringBootTest(properties = "spring.cloud.config.enabled=false" )
 @AutoConfigureMockMvc
 class PractitionerControllerTests {
+
+    @MockBean
+    private PractitionerConfigurationProvider configurationProvider;
+
+    private static List<PractitionerAttributeConfiguration> ALL_REQUIRED_UNDER_35_MAP = List.of(
+        new PractitionerAttributeConfiguration("givenName", "Given Name", "^[a-zA-Z]{1,35}$"),
+        new PractitionerAttributeConfiguration("familyName", "Family Name", "^[a-zA-Z]{1,35}$"),
+        new PractitionerAttributeConfiguration("prefix", "Prefix", "^[a-zA-Z]{1,35}$"));
+
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -36,6 +50,9 @@ class PractitionerControllerTests {
     @Test
     void TestPostPractitioner_WhenNoBody_Returns400() throws Exception {
         // Act + Assert
+        when(configurationProvider.getConfiguration()).thenReturn(new PractitionerConfiguration("person",
+            "Practitioner", ALL_REQUIRED_UNDER_35_MAP));
+
         this.mockMvc.perform(post("/practitioner").contentType(MediaType.APPLICATION_JSON))
                 .andDo(print()).andExpect(status().isBadRequest());
     }
