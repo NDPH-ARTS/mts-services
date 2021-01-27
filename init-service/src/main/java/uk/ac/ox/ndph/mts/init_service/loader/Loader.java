587c10e0262ffb846d04e290e91d5dd437c2243a
Saj <saj.issa@gmail.com>
2021-01-27 18:57:52 +0000

    [ARTS-350] - rename EntityService to ServiceInvoker and assosiated object names

--- init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/loader/Loader.java
+++ init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/loader/Loader.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import uk.ac.ox.ndph.mts.init_service.service.JsonService;
-import uk.ac.ox.ndph.mts.init_service.service.PractitionerService;
-import uk.ac.ox.ndph.mts.init_service.service.RoleService;
-import uk.ac.ox.ndph.mts.init_service.service.SiteService;
+import uk.ac.ox.ndph.mts.init_service.service.PractitionerServiceInvoker;
+import uk.ac.ox.ndph.mts.init_service.service.RoleServiceInvoker;
+import uk.ac.ox.ndph.mts.init_service.service.SiteServiceInvoker;
 
 @Component
 public class Loader implements CommandLineRunner {
@@ -15,19 +15,19 @@ public class Loader implements CommandLineRunner {
     private JsonService jsonService;
 
     @Autowired
-    private PractitionerService practitonerService;
+    private PractitionerServiceInvoker practitionerServiceInvoker;
 
     @Autowired
-    private RoleService roleService;
+    private RoleServiceInvoker roleServiceInvoker;
 
     @Autowired
-    private SiteService siteService;
+    private SiteServiceInvoker siteServiceInvoker;
 
     @Override
     public void run(String... args) {
-        roleService.execute(jsonService.getTrial().getRoles());
-        siteService.execute(jsonService.getTrial().getSites());
-        practitonerService.execute(jsonService.getTrial().getPersons());
+        roleServiceInvoker.execute(jsonService.getTrial().getRoles());
+        siteServiceInvoker.execute(jsonService.getTrial().getSites());
+        practitionerServiceInvoker.execute(jsonService.getTrial().getPersons());
     }
 }
 
