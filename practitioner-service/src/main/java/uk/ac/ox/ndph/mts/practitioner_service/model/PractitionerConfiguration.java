df1e1acdd592c70b5b05962960dc7ee3c66edb33
Archie Maskill <archie.maskill@gmail.com>
2021-01-27 11:55:40 +0000

    ARTS-375: removed Lombok from PractitionerConfiguration

--- practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/model/PractitionerConfiguration.java
+++ practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/model/PractitionerConfiguration.java
@@ -1,24 +1,48 @@
 package uk.ac.ox.ndph.mts.practitioner_service.model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.NoArgsConstructor;
-import java.util.List;
 import org.springframework.stereotype.Component;
-import lombok.AllArgsConstructor;
 
-/**
- * Practitioner service configuration Model
- */
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 @Component
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class PractitionerConfiguration {
-    private String name;
-    private String displayName;
-    private List<PractitionerAttributeConfiguration> attributes;
+
+    private final String name;
+    private final String displayName;
+
+    private final List<PractitionerAttributeConfiguration> attributes;
+
+    public PractitionerConfiguration() {
+        this.name = "";
+        this.displayName = "";
+        attributes = Collections.emptyList();
+    }
+
+    public PractitionerConfiguration(final String name,
+                                     final String displayName,
+                                     final List<PractitionerAttributeConfiguration> attributes) {
+        this.name = name;
+        this.displayName = displayName;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String toString() {
+        // TODO(archiem) throwing exception to find out where this is used (part of Lombok-removal)
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Collection<PractitionerAttributeConfiguration> getAttributes() {
+        return Collections.unmodifiableCollection(attributes);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
