e6c9b97ce48373c946761c8b742b6393cd7bb011
kedarl <lele.kedar@gmail.com>
2021-01-27 09:41:31 +0000

    08 Fixed Review Comments

--- site-service/src/main/java/uk/ac/ox/ndph/mts/site_service/controller/SiteController.java
+++ site-service/src/main/java/uk/ac/ox/ndph/mts/site_service/controller/SiteController.java
@@ -1,6 +1,7 @@
 package uk.ac.ox.ndph.mts.site_service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,9 +13,8 @@ import uk.ac.ox.ndph.mts.site_service.service.SiteService;
  * Controller for site endpoint of site-service
  */
 @RestController()
+@RequestMapping("/sites")
 public class SiteController {
-
-    private static final String ENDPOINT_PATH = "/sites";
 
     private final SiteService siteService;
 
@@ -32,7 +32,7 @@ public class SiteController {
      * @param site
      * @return ResponseEntity
      */
-    @PostMapping(path = ENDPOINT_PATH)
+    @PostMapping()
     @ResponseBody
     public String site(@RequestBody Site site) {
         return siteService.save(site);
