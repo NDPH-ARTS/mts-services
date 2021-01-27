d9f93c099f75661418656548b37f400f7ff17ef1
Archie Maskill <archie.maskill@gmail.com>
2021-01-27 12:10:24 +0000

    ARTS-375: removed Lombok from PractitionerAttributeConfiguration

--- practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/model/PractitionerAttributeConfiguration.java
+++ practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/model/PractitionerAttributeConfiguration.java
@@ -1,24 +1,41 @@
 package uk.ac.ox.ndph.mts.practitioner_service.model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import lombok.AllArgsConstructor;
-
-/**
- * Practitioner Attribute configuration Model
- */
 @Component
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class PractitionerAttributeConfiguration {
-    private String name;
-    private String displayName;
-    private String validationRegex;
+
+    private final String name;
+    private final String displayName;
+    private final String validationRegex;
+
+    public PractitionerAttributeConfiguration() {
+        this.name = "";
+        this.displayName = "";
+        this.validationRegex = "";
+    }
+
+    public PractitionerAttributeConfiguration(final String name, final String displayName, final String validationRegex) {
+        this.name = name;
+        this.displayName = displayName;
+        this.validationRegex = validationRegex;
+    }
+
+    @Override
+    public String toString() {
+        // TODO(archiem) throwing exception to find out where this is used (part of Lombok-removal)
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getValidationRegex() {
+        return validationRegex;
+    }
 }
