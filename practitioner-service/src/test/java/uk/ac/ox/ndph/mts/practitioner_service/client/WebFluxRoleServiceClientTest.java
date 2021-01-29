eb880c0c99e43bace2064edb240ebe9c123c41ec
Alex Baxter <alex.baxter@ndph.ox.ac.uk>
2021-01-29 17:01:17 +0000

    Test classes should be package-scope, fix a code smell

--- practitioner-service/src/test/java/uk/ac/ox/ndph/mts/practitioner_service/client/WebFluxRoleServiceClientTest.java
+++ practitioner-service/src/test/java/uk/ac/ox/ndph/mts/practitioner_service/client/WebFluxRoleServiceClientTest.java
@@ -24,7 +24,7 @@ import static org.junit.Assert.assertThrows;
 
 @SpringBootTest(properties =  { "http.readTimeOutMs=1000", "http.connectTimeOutMs=500" })
 @ContextConfiguration
-public class WebFluxRoleServiceClientTest {
+class WebFluxRoleServiceClientTest {
 
     private TestRolesServiceBackend mockBackEnd;
 
