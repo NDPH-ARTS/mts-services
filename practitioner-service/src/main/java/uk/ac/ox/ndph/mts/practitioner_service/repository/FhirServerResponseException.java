4c67a8dd935b1b2b1077cc2d4f92f22ca31c3cec
Archie Maskill <archie.maskill@gmail.com>
2021-01-30 16:31:28 +0000

    ARTS-473: removing all comments and commented-out code

--- practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/repository/FhirServerResponseException.java
+++ practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/repository/FhirServerResponseException.java
@@ -4,15 +4,8 @@ import ca.uhn.fhir.rest.server.exceptions.BaseServerResponseException;
 
 public class FhirServerResponseException extends Exception {
 
-    // Commenting out to suppress the Sonar code smell.
-    // The idea is that this exception will eventually expose interesting details
-    // about the wrapped runtimeBaseServerResponseException exception.
-    // If we haven't found a use for it after a while, throw it away.
-    //private final BaseServerResponseException ex;
-
     FhirServerResponseException(final String message, BaseServerResponseException ex) {
         super(message, ex);
-//        this.ex = ex;
     }
 
 }
