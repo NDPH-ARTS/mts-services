7180e84b0fa894dc0656c39252a9e4f754912a6b
Archie Maskill <archie.maskill@gmail.com>
2021-01-30 12:36:32 +0000

    ARTS-543: tidied FhirContextWrapper tests

--- practitioner-service/src/test/java/uk/ac/ox/ndph/mts/practitioner_service/repository/FhirContextWrapperTest.java
+++ practitioner-service/src/test/java/uk/ac/ox/ndph/mts/practitioner_service/repository/FhirContextWrapperTest.java
@@ -14,11 +14,12 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class FhirContextWrapperTest {
 
+    final FhirContextWrapper sut = new FhirContextWrapper();
+    final Bundle EMPTY_BUNDLE = new Bundle();
+    final Bundle bundle = new Bundle();
+
     @Test
     void getResourcesFrom_whenNullBundle_thenThrowException() {
-        // Arrange
-        FhirContextWrapper sut = new FhirContextWrapper();
-
         // Act + Assert
         assertThatExceptionOfType(NullPointerException.class)
                 .isThrownBy(() -> sut.getResourcesFrom(null, 1))
@@ -27,23 +28,16 @@ class FhirContextWrapperTest {
 
     @Test
     void getResourcesFrom_whenEmptyBundleAndExpectNonZero_thenThrowException() {
-        // Arrange
-        FhirContextWrapper sut = new FhirContextWrapper();
-        Bundle bundleNoResource = new Bundle();
-
         // Act + Assert
         assertThatExceptionOfType(RestException.class)
-                .isThrownBy(() -> sut.getResourcesFrom(bundleNoResource, 1))
+                .isThrownBy(() -> sut.getResourcesFrom(EMPTY_BUNDLE, 1))
                 .withMessage(String.format(FhirRepo.BAD_RESPONSE_SIZE.message(), 0));
     }
 
     @Test
     void getResourcesFrom_whenEmptyBundleAndExpectZero_thenReturnEmptyCollection() {
-        FhirContextWrapper sut = new FhirContextWrapper();
-        Bundle bundleMultiple = new Bundle();
-
         // Act
-        Collection<IBaseResource> resources = sut.getResourcesFrom(bundleMultiple, 0);
+        Collection<IBaseResource> resources = sut.getResourcesFrom(EMPTY_BUNDLE, 0);
 
         // Assert
         assertThat(resources).isEmpty();
@@ -51,28 +45,26 @@ class FhirContextWrapperTest {
 
     @Test
     void getResourcesFrom_whenNonZeroResourcesAndExpectDifferentNumber_thenThrowException() {
-        FhirContextWrapper sut = new FhirContextWrapper();
-        Bundle bundleMultiple = new Bundle();
-        bundleMultiple.addEntry().setResource(new Practitioner());
-        bundleMultiple.addEntry().setResource(new Practitioner());
+        // Arrange
+        bundle.addEntry().setResource(new Practitioner());
+        bundle.addEntry().setResource(new Practitioner());
 
         // Act + Assert
         assertThatExceptionOfType(RestException.class)
-                .isThrownBy(() -> sut.getResourcesFrom(bundleMultiple, 1))
+                .isThrownBy(() -> sut.getResourcesFrom(bundle, 1))
                 .withMessage(String.format(FhirRepo.BAD_RESPONSE_SIZE.message(), 2));
     }
 
     @Test
     void getResourcesFrom_whenNonZeroResourcesAndExpectSameNumber_thenReturnCorrectList() {
-        FhirContextWrapper sut = new FhirContextWrapper();
-        Bundle bundleMultiple = new Bundle();
+        // Arrange
         final Practitioner PRACTITIONER1 = new Practitioner();
         final Practitioner PRACTITIONER2 = new Practitioner();
-        bundleMultiple.addEntry().setResource(PRACTITIONER1);
-        bundleMultiple.addEntry().setResource(PRACTITIONER2);
+        bundle.addEntry().setResource(PRACTITIONER1);
+        bundle.addEntry().setResource(PRACTITIONER2);
 
         // Act
-        List<IBaseResource> resources = sut.getResourcesFrom(bundleMultiple, 2);
+        List<IBaseResource> resources = sut.getResourcesFrom(bundle, 2);
 
         // Assert
         assertThat(resources).hasSize(2).containsOnlyOnce(PRACTITIONER1).containsOnlyOnce(PRACTITIONER2);
