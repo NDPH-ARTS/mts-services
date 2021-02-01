a2e7aad93295ec8bf55b259bdd65cefde076b993
Alex Baxter <alex.baxter@ndph.ox.ac.uk>
2021-02-01 15:25:00 +0000

    [ARTS-466] Add validation that requested role exists (#94)
    * Web client reading from service, parse is incorrect, test fails
    
    * Now parses response from role-service correctly, test passes though needs to use a mock rather than have the actual role-service running
    
    * * Test now uses a mock server so no dependency on external service, and tests pass
    * Added new test dependencies to the POM
    
    * Refactored the role-service call into dedicated client class
    
    * Role-service client how uses proper API, added test class
    
    * Attempt to fix checkstyle warnings
    
    * Fix whitespace checkstyle warnings
    
    * Fix whitespace checkstyle warnings (indent, IDE autoformat wrong)
    
    * Fixed ref to config parameter (in application.properties)
    
    * * Made the integration test pass using a mock client
    * Added configurable timeouts to the role-service client
    
    * * Try to improve test coverage on the role-service client
    * Refactored out the mock back-end to it's own class, fix a test code smell
    
    * * Set both the timeouts to get coverage on the configuration
    * RoleAssignmentValidation test class now gets a mock client
    
    * Test classes should be package-scope, fix a code smell
    
    * Update to match main
    
    * * Remove unused code
    * Update tests accordingly
    * (After rebase on main)

--- practitioner-service/src/test/java/uk/ac/ox/ndph/mts/practitioner_service/validation/RoleAssignmentValidationTests.java
+++ practitioner-service/src/test/java/uk/ac/ox/ndph/mts/practitioner_service/validation/RoleAssignmentValidationTests.java
@@ -1,19 +1,41 @@
 package uk.ac.ox.ndph.mts.practitioner_service.validation;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.ac.ox.ndph.mts.practitioner_service.NullableConverter;
+import uk.ac.ox.ndph.mts.practitioner_service.client.WebFluxRoleServiceClient;
+import uk.ac.ox.ndph.mts.practitioner_service.exception.RestException;
 import uk.ac.ox.ndph.mts.practitioner_service.model.RoleAssignment;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RoleAssignmentValidationTests {
+
+    @Mock
+    private WebFluxRoleServiceClient roleServiceClient;
+
+    @Captor
+    ArgumentCaptor<String> roleIdCaptor;
+
+    private RoleAssignmentValidation validator;
+
+    @BeforeEach
+    void setup() {
+         this.validator = new RoleAssignmentValidation(roleServiceClient);
+    }
 
     @ParameterizedTest
     @CsvSource({",,,practitionerId",
@@ -29,8 +51,6 @@ class RoleAssignmentValidationTests {
             @ConvertWith(NullableConverter.class) String roleId, String expectedField) {
         // Arrange
         RoleAssignment roleAssignment = new RoleAssignment(practitionerId, siteId, roleId);
-        var validator = new RoleAssignmentValidation();
-
         // Act + Assert
         var result = validator.validate(roleAssignment);
         assertThat(result.isValid(), is(false));
@@ -38,15 +58,46 @@ class RoleAssignmentValidationTests {
     }
 
     @Test
-    void TestRoleAssignmentValidation_WhenValidPractitioner_ReturnsValidResponse() {
+    void TestRoleAssignmentValidation_WhenValidRole_ReturnsValidResponse() {
         // Arrange
-        RoleAssignment roleAssignment = new RoleAssignment("practitionerId", "siteId", "roleId");
-        var validator = new RoleAssignmentValidation();
+        final var roleId = "testRoleId";
+        when(roleServiceClient.roleIdExists(roleId)).thenReturn(true);
+        final var roleAssignment = new RoleAssignment("practitionerId", "siteId", roleId);
+        // Act
+        final var result = validator.validate(roleAssignment);
+        // Assert
+        assertThat(result.isValid(), is(true));
+        Mockito.verify(roleServiceClient).roleIdExists(roleIdCaptor.capture());
+        assertThat(roleId, equalTo(roleIdCaptor.getValue()));
+    }
 
+    @Test
+    void TestRoleAssignmentValidation_WhenInvalidRole_ReturnsInvalidResponse() {
+        // Arrange
+        var roleId = "missingRoleId";
+        when(roleServiceClient.roleIdExists(roleId)).thenReturn(false);
+        final RoleAssignment roleAssignment = new RoleAssignment("practitionerId", "siteId", roleId);
         // Act
         var result = validator.validate(roleAssignment);
         // Assert
-        assertThat(result.isValid(), is(true));
+        assertThat(result.isValid(), is(false));
+        assertThat(result.getErrorMessage(), containsString("roleId"));
+        Mockito.verify(roleServiceClient).roleIdExists(roleIdCaptor.capture());
+        var value = roleIdCaptor.getValue();
+        assertThat(roleId, equalTo(value));
+    }
+
+    @Test
+    void TestRoleAssignmentValidation_WhenServiceFails_ThrowsException() {
+        // Arrange
+        var roleId = "testRoleId";
+        when(roleServiceClient.roleIdExists(roleId)).thenThrow(new RestException("mock"));
+        final RoleAssignment roleAssignment = new RoleAssignment("practitionerId", "siteId", roleId);
+        // Act
+        // Assert
+        assertThrows(RestException.class, () -> validator.validate(roleAssignment));
+        Mockito.verify(roleServiceClient).roleIdExists(roleIdCaptor.capture());
+        assertThat(roleId, equalTo(roleIdCaptor.getValue()));
     }
 
 }
