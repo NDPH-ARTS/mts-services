6def099f874db39861ab050c1a5047eb5742d03b
Sameera Purini <31847267+sameerapurini@users.noreply.github.com>
2021-02-01 15:59:07 +0000

    [ARTS-101] Permissions automation api tests (#91)
    * api tests around permissions, updated readme, moved a global function to utils file
    
    * additional scenarios added
    
    * added Get operation

---
+++ api-tests/data/role-service/permissions.js
@@ -0,0 +1,47 @@
+const utils = require('../../data/common/utils')
+
+const assignPermission = {
+    "id": utils.getRandomString(7),
+    "permissions": [
+        {
+            "id": "create-person"
+        }
+    ]
+}
+
+const multiplePermissions = {
+    "id": utils.getRandomString(7),
+    "permissions": [
+        {
+            "id": "create-person"
+        },
+        {
+            "id": "view-person"
+        }
+    ]
+}
+
+const emptyPermission = {
+    "id": utils.getRandomString(7),
+    "permissions": [
+        {
+            "id": ""
+        }
+    ]
+}
+
+const InvalidPermission = {
+    "id": utils.getRandomString(7),
+    "permissions": [
+        {
+            "id": "not-present"
+        }
+    ]
+}
+
+module.exports = {
+    assignPermission,
+    multiplePermissions,
+    emptyPermission,
+    InvalidPermission,
+}
