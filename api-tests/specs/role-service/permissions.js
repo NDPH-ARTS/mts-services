408d2e210099d09a9f1ce6cc634af6c1ee2ab773
Sameera Purini <purinisameera@gmail.com>
2021-02-02 14:34:59 +0000

    update endpoint uri and delete before each for permissions and practitioner services api tests

--- api-tests/specs/role-service/permissions.js
+++ api-tests/specs/role-service/permissions.js
@@ -1,38 +1,36 @@
 const requests = require('../../data/role-service/permissions')
 const conf = require('../../config/conf')
-
-beforeEach(function () {
-    baseRequest = request(conf.BASE_URL)
-})
+const endpointUri = ':82/roles';
 
 describe('As a user I want to set permissions for a role so that I can decide what functionality users assigned this role will have in the system', function () {
+
     it('User is able to assign a permission to a role', async () => {
-        const response = await baseRequest.post('/roles').send(requests.assignPermission);
+        const response = await baseRequest.post(endpointUri).send(requests.assignPermission);
         expect(response.text).to.contain("id")
         expect(response.status).to.equal(HttpStatus.OK)
     });
 
 
     it('User is able to assign multiple permissions to a role', async () => {
-        const response = await baseRequest.post('/roles').send(requests.multiplePermissions);
+        const response = await baseRequest.post(endpointUri).send(requests.multiplePermissions);
         expect(response.text).to.contain("id")
         expect(response.status).to.equal(HttpStatus.OK)
     });
 
     it('User gets a bad request error when no permission is assigned to a role', async () => {
-        const response = await baseRequest.post('/roles').send(requests.emptyPermission);
+        const response = await baseRequest.post(endpointUri).send(requests.emptyPermission);
         expect(response.status).to.equal(HttpStatus.BAD_REQUEST);
         expect(response.text).to.contain("Permission  not found")
     });
 
     it('User gets a bad request error when a non-existing permission is assigned to a role', async () => {
-        const response = await baseRequest.post('/roles').send(requests.InvalidPermission);
+        const response = await baseRequest.post(endpointUri).send(requests.InvalidPermission);
         expect(response.status).to.equal(HttpStatus.BAD_REQUEST);
         expect(response.text).to.contain("Permission not-present not found")
     });
 
     it('User is able to view the created roles', async () => {
-        const response = await baseRequest.get('/roles?page=0&size=2');
+        const response = await baseRequest.get(`${endpointUri}?page=0&size=2');
         expect(response.status).to.equal(HttpStatus.OK)
         expect(response.text).to.contain("id");
     });
