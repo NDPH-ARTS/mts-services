19e031d433f55b970c5ce189673693f0579aa031
Liza Shakury <42377481+LizaShak@users.noreply.github.com>
2021-02-02 19:13:16 +0200

    [ARTS-292] Authorisation component infrastructure (#77)
    * authorisation infrastructure
    
    * add more documentation
    
    * mark csrf as sensitive
    
    * fix sonarcloud issues
    
    * add tests for authentication entry point
    
    * ignore sonar csrf disable rule
    
    * add tests for authorisation service
    
    * comment out parts that con not be tested
    
    * resolve sonar issues
    
    * CR changes
    
    * remove sonar hot spots
    
    * resolve sonar issues
    
    * resolve sonar issues
    
    * comment out for coverage
    
    * CR changes
    
    * add authN and authZ package.info

---
+++ sample-service/src/main/java/uk/ac/ox/ndph/mts/sample_service/security/authroisation/AuthorisationService.java
@@ -0,0 +1,88 @@
+package uk.ac.ox.ndph.mts.sample_service.security.authroisation;
+
+import org.springframework.stereotype.Service;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * The service which performs the authorisation flow
+ */
+@Service
+public class AuthorisationService {
+
+    /**
+     * Authorise request
+     * @return true if authorised
+     */
+    @SuppressWarnings("squid:S2589") //allow using method that always return true, currently a stub algorithm
+    public boolean authorise(String requiredPermission) {
+        //currently
+        String userId = "stubUserId";
+
+        //get practitioner assignment role
+        List<String> participantRoles = getParticipantRoles(userId);
+
+        //get permissions for the the practitioner assignment roles
+        List<String> permissions = participantRoles.stream().flatMap(role ->
+                getRolePermissions(role).stream()).collect(Collectors.toList());
+
+        //validate the required permission is present
+        if (!permissions.contains(requiredPermission)) {
+            return false;
+        }
+
+        //we will also update the practitioner roles to be only those who have the permissions to perform the action
+        //currently just a stub to show we filtered them out
+
+        List<String> updatedParticipantRoles = participantRoles.subList(0, 1);
+
+        //Get entity site. Currently just a stub
+        String entitySite = "stubSite";
+
+        /** Commenting the following lines out because of sonar test coverage which can't be tested fully on stubs
+         But the code is relevant to the flow
+
+        for (String role :  updatedParticipantRoles) {
+            //Validate that one of the roles has the site that is allowed to work on the entity
+            if (isSiteAuthorised(entitySite, role)) {
+                return true;
+            }
+        }
+         */
+
+        return isSiteAuthorised(entitySite, "stubRole");
+    }
+
+    /**
+     * Get participant roles
+     * @param userId - user id on the token
+     * @return true - currently it is a stub/infra until the implementation will be added
+     */
+    @SuppressWarnings("squid:S1172") //suppress unused parameter
+    private List<String> getParticipantRoles(String userId) {
+        return Arrays.asList("stubRole1", "stubRole2");
+    }
+
+    /**
+     * Get user permissions
+     * @param userRole - participant role
+     * @return true - currently it is a stub/infra until the implementation will be added
+     */
+    @SuppressWarnings("squid:S1172") //suppress unused parameter
+    private List<String> getRolePermissions(String userRole) {
+        return Collections.singletonList("stubPermission");
+    }
+
+    /**
+     * Validate user site trees allowed the entity site
+     * @param userRole - participant role which includes the site property
+     * @param entitySite - the requested entity site
+     * @return true - currently it is a stub/infra until the implementation will be added
+     */
+    @SuppressWarnings("squid:S1172") //suppress unused parameter
+    private boolean isSiteAuthorised(String entitySite, String userRole) {
+        return true;
+    }
+}
---
+++ sample-service/src/main/java/uk/ac/ox/ndph/mts/sample_service/security/authroisation/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The authorisation package of this application
+ */
+package uk.ac.ox.ndph.mts.sample_service.security.authroisation;
