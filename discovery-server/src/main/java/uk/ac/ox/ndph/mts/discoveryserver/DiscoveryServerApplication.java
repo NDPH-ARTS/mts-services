7a0a152a4b8bed470e43b55ce53a618d7997c572
tamirkamara <26870601+tamirkamara@users.noreply.github.com>
2021-02-10 07:46:28 +0200

    refactor POMs

--- discovery-server/src/main/java/uk/ac/ox/ndph/mts/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/uk/ac/ox/ndph/mts/discoveryserver/DiscoveryServerApplication.java
@@ -8,9 +8,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DiscoveryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
 
 }
 
