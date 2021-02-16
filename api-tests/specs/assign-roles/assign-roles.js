32a8fde23ed2e872c4405a419cf6f2bfb2a75a0c
Sameera Purini <purinisameera@gmail.com>
2021-02-16 18:59:27 +0000

    deleted the console logs

--- api-tests/specs/assign-roles/assign-roles.js
+++ api-tests/specs/assign-roles/assign-roles.js
@@ -13,35 +13,27 @@ describe('As a user with Assign Roles permission I want to assign roles to a use
         //get parent site Id from sites endpoint
         const getSitesResponse = await baseRequest.get(sitesEndpointUri);
         const captureParentSiteId = getSitesResponse.text
-        console.log('Captured Parent Site Id is' + captureParentSiteId)
         let parseParentSiteIdData = JSON.parse(captureParentSiteId)
         parentSiteId = parseParentSiteIdData[0].siteId
-        console.log('Parsed Parent Site Id is' + parentSiteId)
 
         //request posted to practitioner end point 
         const personResponse = await baseRequest.post(practitionerEndpointUri).send(requests.createPerson)
         const capturePersonResponseData = personResponse.text
         let parsePersonResponseData = JSON.parse(capturePersonResponseData)
         personId = parsePersonResponseData.id
-        console.log('Parsed Person Id is' + personId)
 
         //request posted to role service endpoint
         const roleResponse = await baseRequest.post(rolesEndpointUri).send(requests.roleWithPermissions)
         const captureRoleResponseData = roleResponse.text
         let parseRoleResponseData = JSON.parse(captureRoleResponseData)
         roleId = parseRoleResponseData.id
-        console.log('Parsed Role Id is' + roleId)
 
         //assign roles to a person
         let assignRoleJSON = requests.assignRole
         assignRoleJSON.siteId = parentSiteId
-        console.log('parentsiteId taken in the request is' + parentSiteId)
         assignRoleJSON.roleId = roleId
-        console.log('roleId taken in the request is' + roleId)
         assignRoleEndpointUri = assignRoleEndpointUri.replace("{personId}", personId);
-        console.log('the post uri is' + assignRoleEndpointUri)
         const response = await baseRequest.post(assignRoleEndpointUri).send(assignRoleJSON)
-        console.log('the final response status is' + response.status)
         expect(response.status).to.equal(HttpStatus.CREATED)
     });
 });
