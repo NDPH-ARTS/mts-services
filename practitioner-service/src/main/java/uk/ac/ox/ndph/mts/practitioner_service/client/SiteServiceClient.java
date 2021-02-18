9f7994b44c519ba7f059fe83b249f0a5a2eee777
Anat Balzam <anatbalzam@microsoft.com>
2021-02-18 10:15:42 +0200

    fix sonar error

--- practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/client/SiteServiceClient.java
+++ practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/client/SiteServiceClient.java
@@ -1,7 +1,6 @@
 package uk.ac.ox.ndph.mts.practitioner_service.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
