e6982dd7a0f72ecfac5427c81daf0405c3433fe5
Ben Deinde <ben.deinde@ndph.ox.ac.uk>
2021-02-20 12:26:51 +0000

    link person test scripts

---
+++ api-tests/data/practitioner-service/linkperson.js
@@ -0,0 +1,35 @@
+const createFirstPerson = {
+    "prefix": "Mr",
+    "givenName": "Adam",
+    "familyName": "Rich"
+}
+
+const createSecondPerson = {
+    "prefix": "Ms",
+    "givenName": "Louise",
+    "familyName": "Gardener"
+}
+
+const createThirdPerson = {
+    "prefix": "Ms",
+    "givenName": "Chelsea",
+    "familyName": "Edwards"
+}
+
+const createFourthPerson = {
+    "prefix": "Dr",
+    "givenName": "Graham",
+    "familyName": "Bell"
+}
+
+const linkUser = {
+    "userAccountId": "user007"
+}
+
+module.exports = {
+    createFirstPerson,
+    createSecondPerson,
+    createThirdPerson,
+    createFourthPerson,
+    linkUser
+};   
