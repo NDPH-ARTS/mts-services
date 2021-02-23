305ff7cf64d122ec34e1152d2b139d10c8ee3ef9
Liza Shakury <42377481+LizaShak@users.noreply.github.com>
2021-02-23 12:26:11 +0200

    [ARTS-360] Validate sites in authorisation (#134)
    * validate sites in authorisation
    
    * add tests
    
    * resolve sonar issues
    
    * add documentation
    
    * Fix Auth documentation
    
    * cr changes
    
    * cr changes#2
    
    * fix lint
    
    * resolve merge issues
    
    * add documentation on reflection
    
    * fix code smell

---
+++ security/src/main/java/uk/ac/ox/ndph/mts/security/authorisation/SiteTreeUtil.java
@@ -0,0 +1,62 @@
+package uk.ac.ox.ndph.mts.security.authorisation;
+
+import org.springframework.stereotype.Component;
+import uk.ac.ox.ndph.mts.client.dtos.SiteDTO;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
+public class SiteTreeUtil {
+
+    /**
+     * Get all subtrees
+     * @param sites list of sites in tree
+     * @return HashMap with siteId as key with value list of SiteDTO in it's subtree
+     */
+    public Map<String, ArrayList<String>> getSiteSubTrees(List<SiteDTO> sites) {
+
+        Map<String, SiteDTO> sitesMap = sites.stream()
+                .collect(Collectors.toMap(SiteDTO::getSiteId, Function.identity()));
+
+        //initial subtrees
+        HashMap<String, ArrayList<String>> sitesSubTrees  = new HashMap<>();
+
+        for (SiteDTO node : sites) {
+            //each subtree should contain the site itself
+            if (!sitesSubTrees.containsKey(node.getSiteId())) {
+                sitesSubTrees.put(node.getSiteId(), new ArrayList<>(Collections.singletonList(node.getSiteId())));
+            }
+            //Add the site to all of it's parents
+            SiteDTO parentNode = sitesMap.get(node.getParentSiteId());
+            while (parentNode != null) {
+                addSiteToParentSite(parentNode, node, sitesSubTrees);
+                parentNode = sitesMap.get(parentNode.getParentSiteId());
+            }
+        }
+
+        return sitesSubTrees;
+    }
+
+    /**
+     * Add site to parent site
+     * @param parentSite to add the site too
+     * @param siteToAdd the site to add
+     * @param sitesSubTrees the subtrees of sites
+     */
+    private void addSiteToParentSite(SiteDTO parentSite,
+                                     SiteDTO siteToAdd,
+                                     HashMap<String, ArrayList<String>> sitesSubTrees) {
+        if (parentSite.getSiteId() != null) {
+            if (!sitesSubTrees.containsKey(parentSite.getSiteId())) {
+                sitesSubTrees.put(parentSite.getSiteId(),
+                        new ArrayList<>(Collections.singletonList(parentSite.getSiteId())));
+            }
+            sitesSubTrees.get(parentSite.getSiteId()).add(siteToAdd.getSiteId());
+        }
+    }
+}
