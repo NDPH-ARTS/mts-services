5dca7c0b78fa1fd1fe6fddf21fae858d8d0855d2
Darren Smithson <33603539+darrengsmithson@users.noreply.github.com>
2021-02-24 21:54:03 +0000

    [ARTS-353] adding init-service token functionality

--- init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/config/TokenService.java
+++ init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/config/TokenService.java
@@ -1,9 +1,5 @@
 package uk.ac.ox.ndph.mts.init_service.config;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.exceptions.JWTDecodeException;
-import com.auth0.jwt.interfaces.DecodedJWT;
-
 public interface TokenService {
 
     /**
@@ -12,25 +8,5 @@ public interface TokenService {
      * @return An OAuth token in string format
      */
     String getToken();
-
-    /**
-     * Gets the token from the cloud provider
-     * Initially we will use the default credential builder in Azure
-     *
-     * @return An OAuth token in string format
-     */
-    default String getIdentityId() {
-        String token = getToken();
-        String identityId = null;
-
-        try {
-            DecodedJWT jwt = JWT.decode(token);
-            identityId = jwt.getId();
-        } catch (JWTDecodeException jwtDecodeException) {
-            //invalid token
-        }
-
-        return identityId;
-    }
 
 }
