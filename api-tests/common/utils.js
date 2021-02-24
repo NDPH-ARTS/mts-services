19bcdec1cd23225b0216a44f793bf6cd1c38e614
Sameera Purini <purinisameera@gmail.com>
2021-02-24 19:14:14 +0000

    token id added to handle authorisation

--- api-tests/common/utils.js
+++ api-tests/common/utils.js
@@ -1,3 +1,8 @@
+let formData = require('form-data');
+const authUri = 'https://login.microsoftonline.com/5d23383f-2acb-448e-8353-4b4573b82276/oauth2/v2.0/token'
+const fetch = require("node-fetch");
+let token_request = require('../config/token-request.json');
+
 class utils {
 
     getRandomString(length) {
@@ -9,6 +14,24 @@ class utils {
         }
         return result;
     }
+
+    getTokenId() {
+        let form = new formData();
+        form.append('grant_type', token_request['grant_type'])
+        form.append('client_id', token_request.client_id)
+        form.append('client_secret', token_request.client_secret)
+        form.append('scope', token_request.scope)
+        form.append('username', token_request.username)
+        form.append('password', token_request.password)
+
+        fetch(authUri, {
+            method: 'POST',
+            body: form
+        })
+            .then(response => response.json())
+            .catch(error => console.error('Error:', error))
+            .then(response => console.log('Success:', JSON.stringify(response.id_token)))
+    };
 }
 
 module.exports = new utils;
