1346b051eff8ec38565b99107d3ddfff002e78f5
tamirkamara <26870601+tamirkamara@users.noreply.github.com>
2021-02-25 21:41:52 +0200

    add a scope

--- init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/config/AzureTokenService.java
+++ init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/config/AzureTokenService.java
@@ -20,7 +20,8 @@ public class AzureTokenService implements TokenService {
 
         TokenRequestContext trc1 = new TokenRequestContext();
         // Ask for a basic scope that usually all users are able to get.
-        trc1.addScopes("https://graph.microsoft.com/.default");
+//        trc1.addScopes("https://graph.microsoft.com/.default");
+        trc1.addScopes("api://mts-dev/default");
 
         return Objects.requireNonNull(tokenCredential.getToken(trc1).block()).getToken();
     }
