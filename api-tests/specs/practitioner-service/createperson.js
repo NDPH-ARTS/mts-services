85591d4e87277f0449f58f94d77b3e68ba0910d0
Sameera Purini <purinisameera@gmail.com>
2021-02-28 16:31:07 +0000

    deleted the debug mode

--- api-tests/specs/practitioner-service/createperson.js
+++ api-tests/specs/practitioner-service/createperson.js
@@ -2,12 +2,12 @@ const requests = require('../../data/practitioner-service/createperson')
 const conf = require('../../config/conf')
 const utils = require('../../common/utils')
 const fetch = require("node-fetch");
-const endpointUri = ':8080/api/practitioner';
+const endpointUri = 'https://as-tauth1-sc-gateway-dev.azurewebsites.net/api/practitioner';
 
 
 describe('As a user with Create Person permission, I want to have my create person request validated by the system, So that I cannot create an invalid person', function () {
 
-    it.only('When I submit an API request to create a Person with a value for all mandatory fields and no fields exceed their specified maximum length, Then a new Person record is persisted in the system with a unique identifier And I receive a success acknowledgement', async () => {
+    it('When I submit an API request to create a Person with a value for all mandatory fields and no fields exceed their specified maximum length, Then a new Person record is persisted in the system with a unique identifier And I receive a success acknowledgement', async () => {
         const headers = await utils.getHeadersWithAuth()
         let fetchResponse = await fetch(endpointUri, {
             headers: headers,
@@ -19,7 +19,7 @@ describe('As a user with Create Person permission, I want to have my create pers
         expect(fetchResponse.status).to.equal(HttpStatus.CREATED);
     });
 
-    it.only('When I submit an API request to create a Person with missing non-mandatory prefix field, Then a new Person record is persisted in the system with a unique identifier And I receive a success acknowledgement', async () => {
+    it('When I submit an API request to create a Person with missing non-mandatory prefix field, Then a new Person record is persisted in the system with a unique identifier And I receive a success acknowledgement', async () => {
         const headers = await utils.getHeadersWithAuth()
         let fetchResponse = await fetch(endpointUri, {
             headers: headers,
@@ -31,7 +31,7 @@ describe('As a user with Create Person permission, I want to have my create pers
         expect(fetchResponse.status).to.equal(HttpStatus.CREATED);
     });
 
-    it.only('When I submit an API request to create a Person with missing non-mandatory givenName field, Then a new Person record is persisted in the system with a unique identifier And I receive a success acknowledgement', async () => {
+    it('When I submit an API request to create a Person with missing non-mandatory givenName field, Then a new Person record is persisted in the system with a unique identifier And I receive a success acknowledgement', async () => {
         const headers = await utils.getHeadersWithAuth()
         let fetchResponse = await fetch(endpointUri, {
             headers: headers,
@@ -43,7 +43,7 @@ describe('As a user with Create Person permission, I want to have my create pers
         expect(fetchResponse.status).to.equal(HttpStatus.CREATED);
     });
 
-    it.only('When I submit an API request to create a Person with missing manadatory familyName field, Then a new Person record is not created And I receive an error notification', async () => {
+    it('When I submit an API request to create a Person with missing manadatory familyName field, Then a new Person record is not created And I receive an error notification', async () => {
         const headers = await utils.getHeadersWithAuth()
         let fetchResponse = await fetch(endpointUri, {
             headers: headers,
@@ -55,7 +55,7 @@ describe('As a user with Create Person permission, I want to have my create pers
         expect(fetchResponse.status).to.equal(HttpStatus.UNPROCESSABLE_ENTITY);
     });
 
-    it.only('When I submit an API request to create a Person with any fields exceeding their specified maximum length, Then a new Person record is not created And I receive an error notification', async () => {
+    it('When I submit an API request to create a Person with any fields exceeding their specified maximum length, Then a new Person record is not created And I receive an error notification', async () => {
         const headers = await utils.getHeadersWithAuth()
         let fetchResponse = await fetch(endpointUri, {
             headers: headers,
@@ -67,7 +67,7 @@ describe('As a user with Create Person permission, I want to have my create pers
         expect(fetchResponse.status).to.equal(HttpStatus.UNPROCESSABLE_ENTITY);
     });
 
-    it.only('When I submit an API request to create a Person with any fields holding illegal characters, Then a new Person record is not created And I receive an error notification', async () => {
+    it('When I submit an API request to create a Person with any fields holding illegal characters, Then a new Person record is not created And I receive an error notification', async () => {
         const headers = await utils.getHeadersWithAuth()
         let fetchResponse = await fetch(endpointUri, {
             headers: headers,
@@ -79,7 +79,7 @@ describe('As a user with Create Person permission, I want to have my create pers
         expect(fetchResponse.status).to.equal(HttpStatus.UNPROCESSABLE_ENTITY);
     });
 
-    it.only('When I submit an API request to create a Person with a malformed JSON, Then a new Person record is not created And I receive an error notification', async () => {
+    it('When I submit an API request to create a Person with a malformed JSON, Then a new Person record is not created And I receive an error notification', async () => {
         const headers = await utils.getHeadersWithAuth()
         let fetchResponse = await fetch(endpointUri, {
             headers: headers,
