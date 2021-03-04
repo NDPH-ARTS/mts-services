8e715ed3fa2b33eaa0ea94983da36b1ddee9ce96
Guy Bertental <guybartal@gmail.com>
2021-03-04 14:28:30 +0200

    get init storage connection string

--- init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/loader/Loader.java
+++ init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/loader/Loader.java
@@ -1,5 +1,7 @@
 package uk.ac.ox.ndph.mts.init_service.loader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Component;
 import uk.ac.ox.ndph.mts.init_service.model.Trial;
 import uk.ac.ox.ndph.mts.init_service.service.PractitionerServiceInvoker;
 import uk.ac.ox.ndph.mts.init_service.service.RoleServiceInvoker;
+import uk.ac.ox.ndph.mts.init_service.service.ServiceInvoker;
 import uk.ac.ox.ndph.mts.init_service.service.SiteServiceInvoker;
 
 import java.util.List;
@@ -19,8 +22,13 @@ public class Loader implements CommandLineRunner {
     private final SiteServiceInvoker siteServiceInvoker;
     private final Trial trialConfig;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Loader.class);
+
     @Value("${delay-start:1}")
     private long delayStartInSeconds;
+
+    @Value("${INIT_STORAGE_ACCOUNT_CONNECTION_STRING}")
+    private String initStorageConnectionString;
 
     @Autowired
     public Loader(Trial trialConfig, PractitionerServiceInvoker practitionerServiceInvoker,
@@ -33,9 +41,10 @@ public class Loader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws InterruptedException {
-        // TODO: remove this temporary fix in this story: https://ndph-arts.atlassian.net/browse/ARTS-362
         // Give the other services some time to come online.
         Thread.sleep(delayStartInSeconds * 1000);
+
+        LOGGER.info("init connection string=" + initStorageConnectionString);
 
         roleServiceInvoker.execute(trialConfig.getRoles());
         List<String> siteIds = siteServiceInvoker.execute(trialConfig.getSites());
