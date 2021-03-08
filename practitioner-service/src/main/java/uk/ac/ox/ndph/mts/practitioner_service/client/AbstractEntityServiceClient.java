0d2298be536efeb92ea2c18ac1caed31e61cd9de
Kate Sant <katesan@ndph.ox.ac.uk>
2021-03-08 17:10:35 +0000

    [ARTS 622] Practitioner-service sends token to site-service (in roleassignment validation)

--- practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/client/AbstractEntityServiceClient.java
+++ practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/client/AbstractEntityServiceClient.java
@@ -1,23 +1,32 @@
 package uk.ac.ox.ndph.mts.practitioner_service.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import uk.ac.ox.ndph.mts.practitioner_service.exception.RestException;
+import uk.ac.ox.ndph.mts.practitioner_service.service.EntityService;
+import uk.ac.ox.ndph.mts.security.authentication.SecurityContextUtil;
 
 import java.util.Objects;
+import java.util.logging.Logger;
 
 public abstract class AbstractEntityServiceClient implements EntityServiceClient {
 
     protected WebClient webClient;
     protected String serviceUrlBase;
     protected String serviceExistsRoute;
+    protected SecurityContextUtil securityContextUtil;
+
+
 
     @SuppressWarnings("ConstantConditions")
     @Override
     public boolean entityIdExists(String id) throws RestException {
+        Logger.getAnonymousLogger().info("entityIDexists:"+serviceExistsRoute+id+" token="+securityContextUtil.getToken());
         Objects.requireNonNull(id, "id must be non-null");
         return webClient.get()
                 .uri(serviceExistsRoute, id)
+                .header("Authorization", "Bearer " + securityContextUtil.getToken())
                 .exchange()
                 .flatMap(clientResponse -> {
                     if (clientResponse.statusCode().is4xxClientError()) {
--- practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/client/SiteServiceClient.java
+++ practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/client/SiteServiceClient.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import uk.ac.ox.ndph.mts.security.authentication.SecurityContextUtil;
 
 @Component
 public class SiteServiceClient extends AbstractEntityServiceClient {
 
     @Autowired
     public SiteServiceClient(final WebClient.Builder webClientBuilder,
-                             @Value("${site.service.uri}") String serviceUrlBase) {
+                             @Value("${site.service.uri}") String serviceUrlBase, SecurityContextUtil securityContextUtil) {
         this.serviceUrlBase = serviceUrlBase;
         this.webClient = webClientBuilder.baseUrl(serviceUrlBase).build();
         this.serviceExistsRoute = "/sites/{siteId}";
+        this.securityContextUtil = securityContextUtil;
     }
 
 }
