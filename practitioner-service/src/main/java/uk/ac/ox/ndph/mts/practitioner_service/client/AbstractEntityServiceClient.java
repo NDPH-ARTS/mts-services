57fcf4ddc144ad45d60dca455d3f6c6fb2e4c457
LizaShak <lishakur@microsoft.com>
2021-03-09 19:42:32 +0200

    change header value

--- practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/client/AbstractEntityServiceClient.java
+++ practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/client/AbstractEntityServiceClient.java
@@ -1,5 +1,6 @@
 package uk.ac.ox.ndph.mts.practitioner_service.client;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import uk.ac.ox.ndph.mts.practitioner_service.exception.RestException;
@@ -19,7 +20,7 @@ public abstract class AbstractEntityServiceClient implements EntityServiceClient
         Objects.requireNonNull(id, "id must be non-null");
         return webClient.get()
                 .uri(serviceExistsRoute, id)
-                .header("Authorization", "Bearer " + token)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .exchange()
                 .flatMap(clientResponse -> {
                     if (clientResponse.statusCode().is4xxClientError()) {
