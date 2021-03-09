300897dd97d51beff2e6e433d8ad428534cfd7d7
LizaShak <lishakur@microsoft.com>
2021-03-09 13:38:48 +0200

    update role auth

--- practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/controller/PractitionerController.java
+++ practitioner-service/src/main/java/uk/ac/ox/ndph/mts/practitioner_service/controller/PractitionerController.java
@@ -67,7 +67,7 @@ public class PractitionerController {
         return ResponseEntity.ok(entityService.findPractitionerById(id));
     }
 
-    @PreAuthorize("@authorisationService.authorise('assign-role')") //NOSONAR
+    @PreAuthorize("@authorisationService.authorise('assign-role', #roleAssignment.siteId)") //NOSONAR
     @PostMapping(path = "/{practitionerId}/roles")
     public ResponseEntity<Response> saveRoleAssignment(@PathVariable String practitionerId,
                                                        @RequestBody RoleAssignment roleAssignment) {
