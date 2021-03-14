7ba0590fd5fa330b0f5b107a4ad02f9164de057a
Guy Bertental <guybartal@gmail.com>
2021-03-14 12:43:26 +0200

    remark to test if docker build fails

--- init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/service/InitProgressService.java
+++ init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/service/InitProgressService.java
@@ -3,7 +3,6 @@ package uk.ac.ox.ndph.mts.init_service.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
