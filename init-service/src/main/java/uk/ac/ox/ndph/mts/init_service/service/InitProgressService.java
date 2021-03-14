a05c3791afd520b82d7b65e752326abf8ebc00fd
Guy Bertental <guybartal@gmail.com>
2021-03-14 12:49:01 +0200

    fix bug in file path validation

--- init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/service/InitProgressService.java
+++ init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/service/InitProgressService.java
@@ -3,6 +3,7 @@ package uk.ac.ox.ndph.mts.init_service.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -14,7 +15,9 @@ public class InitProgressService implements AutoCloseable {
 
     @Autowired
     public InitProgressService(@Value("${INIT_PROGRESS_LOG_PATH:}") String initLogMountPath) throws IOException {
-        writer = new BufferedWriter(new FileWriter(initLogMountPath, true));
+        if (StringUtils.hasText(initLogMountPath)) {
+            writer = new BufferedWriter(new FileWriter(initLogMountPath, true));
+        }
     }
 
     public void submitProgress(String text) throws IOException {
