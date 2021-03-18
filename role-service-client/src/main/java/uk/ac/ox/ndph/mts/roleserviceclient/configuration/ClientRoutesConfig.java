44e19a846f9c232e411315df73f3ac19513c1052
Anat Balzam <anatbalzam@microsoft.com>
2021-03-18 17:57:05 +0200

    Fix code smell

--- role-service-client/src/main/java/uk/ac/ox/ndph/mts/roleserviceclient/configuration/ClientRoutesConfig.java
+++ role-service-client/src/main/java/uk/ac/ox/ndph/mts/roleserviceclient/configuration/ClientRoutesConfig.java
@@ -4,33 +4,39 @@ import org.springframework.stereotype.Component;
 //TODO: Make this class a spring @Configuration class with @Value annotation
 @Component
 public class ClientRoutesConfig {
-
+    static final String SERVICE_NAME = "role-service";
+    static final String SERVICE_EXISTS_ROUTE = "/roles/{id}";
+    static final String SERVICE_GET_ROLE = "/roles/{id}";
+    static final String SERVICE_GET_PAGED = "/roles/";
+    static final String SERVICE_ROLES_BY_IDS = "roles";
+    static final String SERVICE_CREATE_ROLE =  "/roles";
+    static final String SERVICE_UPDATE_PERMISSIONS = "/roles/{id}/permissions";
     public String getServiceName() {
-        return "role-service";
+        return SERVICE_NAME;
     }
 
     public String getServiceExistsRoute() {
-        return "/roles/{id}";
+        return SERVICE_EXISTS_ROUTE;
     }
 
     public String getServiceGetRole() {
-        return "/roles/{id}";
+        return SERVICE_GET_ROLE;
     }
 
     public String getServiceGetPaged() {
-        return "/roles/";
+        return SERVICE_GET_PAGED;
     }
 
     public String getServiceRolesByIds() {
-        return "roles";
+        return SERVICE_ROLES_BY_IDS;
     }
 
     public String getServiceCreateRole() {
-        return "/roles";
+        return SERVICE_CREATE_ROLE;
     }
 
     public String getServiceUpdatePermissions() {
-        return "/roles/{id}/permissions";
+        return SERVICE_UPDATE_PERMISSIONS;
     }
 
 }
