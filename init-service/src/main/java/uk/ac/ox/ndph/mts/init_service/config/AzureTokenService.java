154d4ecbfaa9447f24ce18b8f419623c374526dd
LizaShak <lishakur@microsoft.com>
2021-03-21 11:56:41 +0200

    update scope

--- init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/config/AzureTokenService.java
+++ init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/config/AzureTokenService.java
@@ -3,6 +3,8 @@ package uk.ac.ox.ndph.mts.init_service.config;
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.credential.TokenRequestContext;
 import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.identity.ManagedIdentityCredential;
+import com.azure.identity.ManagedIdentityCredentialBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -16,11 +18,12 @@ public class AzureTokenService implements TokenService {
 
     @Override
     public String getToken() {
-        TokenCredential tokenCredential = new DefaultAzureCredentialBuilder().build();
+        TokenCredential tokenCredential = new DefaultAzureCredentialBuilder()
+                .build();
 
         TokenRequestContext trc1 = new TokenRequestContext();
         // Ask for a basic scope that usually all users are able to get.
-        trc1.addScopes("api://mts-dev/default");
+        trc1.addScopes("api://fa5cde1d-d6f8-4d13-9fa4-4d7a374cb290/.default");
 
         return Objects.requireNonNull(tokenCredential.getToken(trc1).block()).getToken();
     }
