dea492af6ab5be226d702de91ad2afa40d20e37e
Sameera Purini <purinisameera@gmail.com>
2021-03-24 16:44:38 +0000

    regression tests for arts-621

--- api-tests/data/assign-roles/assign-roles.js
+++ api-tests/data/assign-roles/assign-roles.js
@@ -19,16 +19,82 @@ const roleWithPermissions = {
     ]
 }
 
+const roleWithSuperUserPermissions = {
+    "id": utils.getRandomString(7),
+    "permissions": [
+        {
+            "id": "create-person"
+        },
+        {
+            "id": "view-person"
+        },
+        {
+            "id": "assign-role"
+        }
+    ]
+}
+
+const roleWithOutSuperUserPermissions = {
+    "id": utils.getRandomString(7),
+    "permissions": [
+        {
+            "id": "create-person"
+        }
+    ]
+}
+
+const regionA = {
+    "name": utils.getRandomString(5),
+    "alias": utils.getRandomString(5),
+    "parentSiteId": "",
+    "siteType": "REGION"
+}
+
+const countryA = {
+    "name": utils.getRandomString(5),
+    "alias": utils.getRandomString(5),
+    "parentSiteId": "",
+    "siteType": "COUNTRY"
+}
+
+const regionB = {
+    "name": utils.getRandomString(5),
+    "alias": utils.getRandomString(5),
+    "parentSiteId": "",
+    "siteType": "REGION"
+}
+
+const countryB = {
+    "name": utils.getRandomString(5),
+    "alias": utils.getRandomString(5),
+    "parentSiteId": "",
+    "siteType": "COUNTRY"
+}
+
+const adminUser1 = {
+    "prefix": "Ms",
+    "givenName": utils.getRandomString(5),
+    "familyName": utils.getRandomString(5),
+    "userAccountId": "d4b73ba8-9d76-408b-b6e1-e3b8953b39e7"
+}
+
+const staff = {
+    "prefix": "Ms",
+    "givenName": utils.getRandomString(5),
+    "familyName": utils.getRandomString(5),
+    "userAccountId": "f4c3120a-1ca4-4b59-ab6d-e362016f68fb"
+}
+
+const linkPersonRoleAtSite = {
+    "practitionerId": "",
+    "siteId": "",
+    "roleId": ""
+}
+
 const createPerson = {
     "prefix": "Ms",
     "givenName": "Cookie",
     "familyName": "Cookie"
-}
-
-const createCCO = {
-    "name": utils.getRandomString(5),
-    "alias": utils.getRandomString(3),
-    "siteType": "CCO"
 }
 
 const createSite = {
@@ -48,6 +114,14 @@ module.exports = {
     createPerson,
     createSite,
     assignRole,
-    createCCO
+    regionA,
+    regionB,
+    countryA,
+    countryB,
+    roleWithSuperUserPermissions,
+    roleWithOutSuperUserPermissions,
+    adminUser1,
+    linkPersonRoleAtSite,
+    staff
 };
 
