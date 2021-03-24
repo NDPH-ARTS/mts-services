559685c46b32a312621fc59bac6881e942aa9244
Anat Balzam <anatbalzam@microsoft.com>
2021-03-24 14:51:07 +0200

    Refactoring the client to be more generic

--- init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/InitServiceApplication.java
+++ init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/InitServiceApplication.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan({"uk.ac.ox.ndph.mts.roleserviceclient"})
 public class InitServiceApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(InitServiceApplication.class);
