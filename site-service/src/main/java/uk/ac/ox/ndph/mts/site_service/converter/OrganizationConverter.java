9123e1f60fdab99b7ab1d46290a5f00c93ef2570
Darren Smithson <33603539+darrengsmithson@users.noreply.github.com>
2021-04-11 11:28:10 +0100

    [ARTS-774] Only retrieve sites for admin role

--- site-service/src/main/java/uk/ac/ox/ndph/mts/site_service/converter/OrganizationConverter.java
+++ site-service/src/main/java/uk/ac/ox/ndph/mts/site_service/converter/OrganizationConverter.java
@@ -59,8 +59,6 @@ public class OrganizationConverter implements EntityConverter<Site, org.hl7.fhir
 
         if (input.getDescription() != null) {
             fhirOrganization.getText().getDiv().setName("description");
-            fhirOrganization.getText().getDiv().setValue(input.getDescription());
-            fhirOrganization.getText().getDiv().setContent(input.getDescription());
             fhirOrganization.getText().getDiv().addText(input.getDescription());
         }
 
