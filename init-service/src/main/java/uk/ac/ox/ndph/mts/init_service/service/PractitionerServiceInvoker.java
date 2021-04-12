c2204ffceae349c08240b09af6c847a9f3b5e3bb
Anat Balzam <anatbaz@gmail.com>
2021-04-12 10:46:28 +0300

    [ARTS-687][Subfeature] Init use practitioner-service-client (#245)
    * Initial implementation of site service client
    
    * Stubs for tests
    
    * Implement tests - part 1
    
    * Revert back security
    
    * Fix checkstyle
    
    * Add missing file from rebase
    
    * Fixing the PR comments
    
    * Fixing the PR comments
    
    * Remove the createMany from role client and move it to init
    
    * Remove the createMany from role client and move it to init
    
    * Remove the createMany from role client and move it to init
    
    * Remove the createMany from role client and move it to init
    
    * Remove the createMany from role client and move it to init - Tests to complete
    
    * Remove the createMany from role client and move it to init - Tests to complete
    
    * Remove the createMany from role client and move it to init - Tests to complete
    
    * Remove the createMany from role client and move it to init - Test completed
    
    * Remove the createMany from role client and move it to init - Test completed
    
    * PR comments + fix of code smell
    
    * PR comments + fix of code smell
    
    * Fix coverage
    
    * Fix coverage
    
    * Fix coverage
    
    * Fix coverage
    
    * Fix coverage
    
    * Fix conflicts after merge
    
    * Initial practitioner-client + tests stubs
    
    * tests complete
    
    * Add the client in pom.xml + name changes
    
    * Correct validation pom.xml
    
    * roll back
    
    * roll back
    
    * pom.xml changes
    
    * pom.xml changes
    
    * dockerfile changes
    
    * Fix typos
    
    * Fix code smells
    
    * Fix content-type header
    
    * Optimize scan
    
    Co-authored-by: Anat Balzam <anatbalzam@microsoft.com>

--- init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/service/PractitionerServiceInvoker.java
+++ init-service/src/main/java/uk/ac/ox/ndph/mts/init_service/service/PractitionerServiceInvoker.java
@@ -3,81 +3,57 @@ package uk.ac.ox.ndph.mts.init_service.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import org.springframework.web.reactive.function.client.WebClient;
 import uk.ac.ox.ndph.mts.init_service.config.AzureTokenService;
-import uk.ac.ox.ndph.mts.init_service.exception.DependentServiceException;
 import uk.ac.ox.ndph.mts.init_service.exception.NullEntityException;
-import uk.ac.ox.ndph.mts.init_service.model.Entity;
-import uk.ac.ox.ndph.mts.init_service.model.IDResponse;
-import uk.ac.ox.ndph.mts.init_service.model.Practitioner;
-import uk.ac.ox.ndph.mts.init_service.model.PractitionerUserAccount;
-import uk.ac.ox.ndph.mts.init_service.model.RoleAssignment;
+import uk.ac.ox.ndph.mts.practitionerserviceclient.PractitionerServiceClient;
+import uk.ac.ox.ndph.mts.practitionerserviceclient.model.PractitionerDTO;
+import uk.ac.ox.ndph.mts.practitionerserviceclient.model.PractitionerUserAccountDTO;
+import uk.ac.ox.ndph.mts.practitionerserviceclient.model.RoleAssignmentDTO;
 
 import java.util.List;
+import java.util.function.Consumer;
+
 
 @Service
-public class PractitionerServiceInvoker extends ServiceInvoker {
+public class PractitionerServiceInvoker {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(PractitionerServiceInvoker.class);
-
-    @Value("${practitioner-service.routes.create}")
-    private String createEndpoint;
-
-    @Value("${practitioner-service.routes.assign-role}")
-    private String assignRoleEndpoint;
-
-    @Value("${practitioner-service.routes.link-user-account}")
-    private String linkUserAccountEndpoint;
+    private PractitionerServiceClient practitionerServiceClient;
+    private AzureTokenService azureTokenService;
 
     @Autowired
-    public PractitionerServiceInvoker(final WebClient.Builder webClientBuilder,
-                              @Value("${practitioner-service.uri}") String serviceUrlBase,
-                              AzureTokenService azureTokenservice) {
-        super(webClientBuilder, serviceUrlBase, azureTokenservice);
+    public PractitionerServiceInvoker(final PractitionerServiceClient practitionerServiceClient,
+                                      AzureTokenService azureTokenService) {
+        this.practitionerServiceClient = practitionerServiceClient;
+        this.azureTokenService = azureTokenService;
     }
 
-    @Override
-    protected String create(Entity practitioner) throws DependentServiceException {
-        String uri = serviceUrlBase + createEndpoint;
-        IDResponse response = sendBlockingPostRequest(uri, practitioner, IDResponse.class);
-        return response.getId();
-    }
-
-    protected void assignRoleToPractitioner(RoleAssignment roleAssignment) throws DependentServiceException {
-        String uri = serviceUrlBase + String.format(assignRoleEndpoint, roleAssignment.getPractitionerId());
-        sendBlockingPostRequest(uri, roleAssignment, IDResponse.class);
-    }
-
-    protected void linkUserAccount(PractitionerUserAccount userAccount) throws DependentServiceException {
-        String uri = serviceUrlBase + String.format(linkUserAccountEndpoint, userAccount.getPractitionerId());
-        sendBlockingPostRequest(uri, userAccount, IDResponse.class);
-    }
-
-    public void execute(List<Practitioner> practitioners, String siteId) throws NullEntityException {
+    public void execute(List<PractitionerDTO> practitioners, String siteId) throws NullEntityException {
         if (practitioners == null) {
             throw new NullEntityException("No Practitioners in payload");
         }
-
-        for (Practitioner practitioner : practitioners) {
+        Consumer<HttpHeaders> authHeaders = PractitionerServiceClient.bearerAuth(azureTokenService.getToken());
+        for (PractitionerDTO practitioner : practitioners) {
             LOGGER.info("Starting to create practitioner(s): {}", practitioner);
-            String practitionerId = create(practitioner);
+            String practitionerId = practitionerServiceClient.createEntity(practitioner, authHeaders).getId();
 
             if (practitioner.getRoles() != null) {
                 LOGGER.info("Assigning roles to practitioner(s): {} {}", practitionerId, practitioner.getRoles());
                 for (String roleId : practitioner.getRoles()) {
-                    RoleAssignment ra = new RoleAssignment(practitionerId, siteId, roleId);
-                    assignRoleToPractitioner(ra);
+                    RoleAssignmentDTO ra = new RoleAssignmentDTO(practitionerId, siteId, roleId);
+                    practitionerServiceClient.assignRoleToPractitioner(ra, authHeaders);
                 }
             }
 
             if (StringUtils.hasText(practitioner.getUserAccount())) {
                 LOGGER.info("Linking practitioner: {} to user account: {}",
                         practitionerId, practitioner.getUserAccount());
-                PractitionerUserAccount userAccount = new PractitionerUserAccount(practitionerId,
+                PractitionerUserAccountDTO userAccount = new PractitionerUserAccountDTO(practitionerId,
                         practitioner.getUserAccount());
-                linkUserAccount(userAccount);
+                practitionerServiceClient.linkUserAccount(userAccount, authHeaders);
             } else {
                 LOGGER.warn("Practitioner {} with no user account won't be able to login", practitionerId);
             }
