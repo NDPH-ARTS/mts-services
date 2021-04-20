c3173f0efbaab5bc5a5559b7fd4416ab0523383d
Anat Balzam <anatbalzam@microsoft.com>
2021-04-20 22:37:11 +0300

    Step 2 B - * Added the new service to the Gateway * Refactor the api-test

--- api-tests/specs/handoff-service/handoff.js
+++ api-tests/specs/handoff-service/handoff.js
@@ -7,19 +7,15 @@ const conf = require('../../config/conf')
 const utils = require('../../common/utils')
 const fetch = require("node-fetch");
 const { expect } = require('chai');
-const endpointUri = '/api/practitioner/profile';
+const endpointUri = '/api/handoff/hello';
 
 
-describe('A Trial is being configured with a bootstrap user assigned to a site and a role with permissions', function () {
+describe('Hello test', function () {
 
-    it('User when pointed to profile endpoint uri gets a successful response with authorisation checks', async () => {
-        const headers = await utils.getHeadersWithAuth()
+    it('Hello test', async () => {
         let fetchResponse = await fetch(conf.baseUrl + endpointUri, {
-            headers: headers,
             method: 'GET',
         })
-        let response = await fetchResponse.json();
         expect(fetchResponse.status).to.equal(HttpStatus.OK);
-        expect(response[0]).to.have.property('id');
     });
 });
