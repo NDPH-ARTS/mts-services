da4a5ad7fb19986cc6e25d965533a7672581599b
Saj <saj.issa@gmail.com>
2021-04-21 22:53:27 +0100

    [saj-hack] - modify test

--- saj-service/src/test/java/uk/ac/ox/ndph/mts/saj_service/controllers/ControllerTests.java
+++ saj-service/src/test/java/uk/ac/ox/ndph/mts/saj_service/controllers/ControllerTests.java
@@ -4,25 +4,34 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import uk.ac.ox.ndph.mts.saj_service.config.ConfigService;
 
-@SpringBootTest(properties = { "spring.cloud.config.discovery.enabled = false" , "spring.cloud.config.enabled=false", "server.error.include-message=always", "spring.main.allow-bean-definition-overriding=true" })
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(properties = {"server.error.include-message=always",
+        "spring.main.allow-bean-definition-overriding=true"})
+@ActiveProfiles({"local", "no-authZ"})
 @AutoConfigureMockMvc
-class ControllerTests {
+class MainControllerTests {
+
+    @MockBean
+    private ConfigService configService;
 
     @Autowired
     private MockMvc mockMvc;
 
+    @WithMockUser
     @Test
-    void TestHello() throws Exception {
-        // Act + Assert
+    void TestHelloEndpoint() throws Exception {
         // Act + Assert
         this.mockMvc.perform(get("/hello"))
                 .andDo(print())
-                .andExpect(status().isUnauthorized());
+                .andExpect(status().isOk());
     }
-
 }
