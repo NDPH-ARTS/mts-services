c57589bbb99f018a8364a91be96e616423e85ebf
Kate Sant <katesan@ndph.ox.ac.uk>
2021-04-26 16:31:41 +0100

    emulate workflow action : test - init

--- local-ci-env.sh
+++ local-ci-env.sh
@@ -7,18 +7,28 @@
 declare GHCR="ghcr.io/ndph-arts"
 declare -a services=( "practitioner-service" "site-service"  "role-service" "init-service" "config-server" "discovery-server" "gateway-server")  #Equivalent of strategy.matrix.service_name in docker-build-push.yml workflow
 
+
 build(){
   for service in "${services[@]}"
   do
     tag=$GHCR/$service:$GITHUB_SHA
-    echo "building $service $tag"
-    docker build --build-arg SVC="$service" -t "$tag" .
+    echo "Build $service $tag"
+    #docker build --build-arg SVC="$service" -t "$tag" .
   done
 }
 
 test(){
-  echo "Start services using Docker Compose"
+
+  export PROFILE="dev"
+
+  echo "Start services"
   .ci/docker/check-docker-compose-services.sh
+
+  echo "Run init"
+  docker-compose run --no-deps init-service
+
+
+
 }
 
 build
