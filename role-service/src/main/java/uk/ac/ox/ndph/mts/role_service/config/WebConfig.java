e6801c344313372d356758c992718b0b54191c4a
Kate Sant <katesan@ndph.ox.ac.uk>
2021-05-07 10:34:21 +0100

    remove default Apache licence declaration - this is being decided still

--- role-service/src/main/java/uk/ac/ox/ndph/mts/role_service/config/WebConfig.java
+++ role-service/src/main/java/uk/ac/ox/ndph/mts/role_service/config/WebConfig.java
@@ -1,5 +1,8 @@
 package uk.ac.ox.ndph.mts.role_service.config;
 
+import io.swagger.annotations.Info;
+import io.swagger.annotations.License;
+import io.swagger.annotations.SwaggerDefinition;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +11,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -20,25 +24,39 @@ import springfox.documentation.swagger.web.UiConfigurationBuilder;
 
 import java.util.ArrayList;
 
+import static springfox.documentation.builders.PathSelectors.any;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.swagger.web.UiConfiguration.Constants.NO_SUBMIT_METHODS;
 
 
 @Configuration
+
 public class WebConfig implements WebMvcConfigurer {
 
 
     @Bean
     public Docket enableSwagger() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(swaggerDisplayOptions())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("uk.ac.ox.ndph.mts.role_service.controller"))
-                .paths(PathSelectors.any())
+                .apis(basePackage("uk.ac.ox.ndph.mts.role_service.controller"))
+                .paths(any())
                 .build();
     }
 
     @Bean
     public UiConfiguration removeSwaggerTryItOutButton() {
-        return UiConfigurationBuilder.builder().supportedSubmitMethods(NO_SUBMIT_METHODS).build();
+        return UiConfigurationBuilder.builder()
+                .supportedSubmitMethods(NO_SUBMIT_METHODS)
+                .build();
+    }
+
+    private ApiInfo swaggerDisplayOptions() {
+        return new ApiInfoBuilder()
+                .title("Role Service API Documentation")
+                .license("") // otherwise this defaults to Apache
+                .licenseUrl("")
+                .build();
     }
 
     @Bean
