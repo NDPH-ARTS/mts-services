798dbc8950a71318213ceda6e3b27cf667049665
Kate Sant <katesan@ndph.ox.ac.uk>
2021-05-07 14:04:51 +0100

    more checkstyle

--- role-service/src/main/java/uk/ac/ox/ndph/mts/role_service/config/WebConfig.java
+++ role-service/src/main/java/uk/ac/ox/ndph/mts/role_service/config/WebConfig.java
@@ -1,32 +1,10 @@
 package uk.ac.ox.ndph.mts.role_service.config;
 
-import io.swagger.annotations.Info;
-import io.swagger.annotations.License;
-import io.swagger.annotations.SwaggerDefinition;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger.web.UiConfigurationBuilder;
-
-import java.util.ArrayList;
-
-import static springfox.documentation.builders.PathSelectors.any;
-import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
-import static springfox.documentation.swagger.web.UiConfiguration.Constants.NO_SUBMIT_METHODS;
 
 
 @Configuration
